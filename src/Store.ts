dc5b1bc9a104590738a98d1f5915b34824f756e5
daheeahn <deg9810@likelion.org>
2020-03-31 13:31:46 +0900

    #4.1 createAction

--- src/Store.ts
+++ src/Store.ts
@@ -1,31 +1,35 @@
 import {createStore} from 'redux';
+import {createAction} from '@reduxjs/toolkit';
 
 export const ADD_TODO = 'ADD_TODO';
 export const DELETE_TODO = 'DELETE_TODO';
 
-// actionCreator
-const addToDo = text => {
-  return {
-    type: ADD_TODO,
-    text,
-  };
-};
+const addToDo = createAction(ADD_TODO);
+const deleteToDo = createAction(DELETE_TODO);
+// console.log(addToDo(), deleteToDo);
 
-const deleteToDo = id => {
-  return {
-    type: DELETE_TODO,
-    id,
-  };
-};
+// actionCreator
+// const addToDo = text => {
+//   return {
+//     type: ADD_TODO,
+//     text,
+//   };
+// };
+// const deleteToDo = id => {
+//   return {
+//     type: DELETE_TODO,
+//     id,
+//   };
+// };
 
 const reducer = (state = [], action) => {
   // state: currentState
   switch (action.type) {
-    case ADD_TODO:
-      const newToDoObj = {text: action.text, id: Date.now()};
+    case addToDo.type:
+      const newToDoObj = {text: action.payload, id: Date.now()}; // payload로 정보가 온다. (관행)
       return [newToDoObj, ...state];
-    case DELETE_TODO:
-      const cleaned = state.filter(s => s.id !== action.id);
+    case deleteToDo.type:
+      const cleaned = state.filter(s => s.id !== action.payload);
       return cleaned;
     default:
       return state;
